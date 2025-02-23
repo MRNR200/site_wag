f2bba582ca5c03de293c929d65b4ac2af0415d9c
MRNF <mohammadrezanajafianfar@gmail.com>
2025-02-23 23:23:08 +0330

    Initial commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-g2n@$wtf=a3s8u)@+0muax2u(!knyo9pd5)@f+0ss6-g-r56)u"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
